e051beb8e3ec5e2ebb62ac59da27fcc19529c9c3
nadiakasturi <nadiakasturi21@gmail.com>
2025-04-26 17:05:03 +0700

    re

--- todo-app/next.config.mjs
+++ todo-app/next.config.mjs
@@ -1,9 +1,5 @@
-// next.config.mjs
 export default {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,  // Aktifkan App Router
-  },
   webpack(config) {
     config.module.exprContextCritical = false; // Hapus peringatan yang tidak diperlukan
     return config;
