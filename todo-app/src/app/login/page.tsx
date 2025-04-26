8bc979153c159680ad237aca8488873fca136b6f
nadiakasturi <nadiakasturi21@gmail.com>
2025-04-26 21:52:28 +0700

    rev

--- todo-app/src/app/login/page.tsx
+++ todo-app/src/app/login/page.tsx
@@ -74,7 +74,7 @@ export default function LoginPage() {
 
           <div className="mt-4 text-center">
             <p className="text-sm">
-              Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
               <a href="/register" className="text-blue-500 hover:underline">
                 Register here
               </a>
