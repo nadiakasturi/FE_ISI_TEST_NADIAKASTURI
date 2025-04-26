6dff02619e8a2137259242f614ba8f2abd4fb610
nadiakasturi <nadiakasturi21@gmail.com>
2025-04-26 17:35:39 +0700

    te

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,14 +1,10 @@
 // app/page.tsx
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/register"); // Redirect otomatis ke /register
-  }, [router]);
-
-  return null; // Tidak ada tampilan di halaman utama
-}
+    return (
+      <div>
+        <h1>Welcome to My App!</h1>
+        <p>This is the homepage.</p>
+      </div>
+    );
+  }
+  
